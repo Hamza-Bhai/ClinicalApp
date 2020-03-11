fa9f5efe84f95b1d9ffdffa92d8be3d6dc7d633c
hamza-bhai <hm6048884hm@gmail.com>
2020-03-11 12:42:28 +0500

    Updates

---
+++ precache-manifest.0bf38d95d4ec9e9e92b4b1f3c05b4748.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873e18cd3e0f78c3de5d37d42452b094",
+    "url": "/ClinicalApp/index.html"
+  },
+  {
+    "revision": "5c6452d2522caf68e240",
+    "url": "/ClinicalApp/static/css/2.026d319b.chunk.css"
+  },
+  {
+    "revision": "3172aa6f6f7f2782659d",
+    "url": "/ClinicalApp/static/css/main.220c3c08.chunk.css"
+  },
+  {
+    "revision": "5c6452d2522caf68e240",
+    "url": "/ClinicalApp/static/js/2.701f0f06.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/ClinicalApp/static/js/2.701f0f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3172aa6f6f7f2782659d",
+    "url": "/ClinicalApp/static/js/main.dc6c602b.chunk.js"
+  },
+  {
+    "revision": "424bf5790e5eabe7df26",
+    "url": "/ClinicalApp/static/js/runtime-main.9ee791e0.js"
+  },
+  {
+    "revision": "a44ed3898c77f8434e13103fb85a416d",
+    "url": "/ClinicalApp/static/media/1.a44ed389.jpg"
+  },
+  {
+    "revision": "21ade77eed5afe238a1d174a55cecad5",
+    "url": "/ClinicalApp/static/media/2.21ade77e.jpg"
+  },
+  {
+    "revision": "fb393370c1e11cbfe356b4ba311335f4",
+    "url": "/ClinicalApp/static/media/3.fb393370.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ClinicalApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ClinicalApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ClinicalApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ClinicalApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ClinicalApp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7dd37eabeff55be41266c01a1205bbdf",
+    "url": "/ClinicalApp/static/media/news1.7dd37eab.jpg"
+  },
+  {
+    "revision": "703859f3760b00545de506fddd7d2a47",
+    "url": "/ClinicalApp/static/media/news2.703859f3.jpg"
+  },
+  {
+    "revision": "08b8e58b4358828bf3acca93f4f3adcd",
+    "url": "/ClinicalApp/static/media/news3.08b8e58b.jpg"
+  },
+  {
+    "revision": "e7553b4d333deeed5f63ee5d8e24d0cc",
+    "url": "/ClinicalApp/static/media/patient1.e7553b4d.jpg"
+  },
+  {
+    "revision": "9524ad8de5079beb82a38de0117eb6db",
+    "url": "/ClinicalApp/static/media/patient2.9524ad8d.jpg"
+  },
+  {
+    "revision": "c16f2ea81598b25590bb30d36c206f3b",
+    "url": "/ClinicalApp/static/media/patient3.c16f2ea8.jpg"
+  },
+  {
+    "revision": "185936f9f2d5af3f6f8d082b83307a53",
+    "url": "/ClinicalApp/static/media/patient5.185936f9.jpg"
+  },
+  {
+    "revision": "77a6f9b38434ace5a77d0c56e3a0b45d",
+    "url": "/ClinicalApp/static/media/scope.77a6f9b3.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ClinicalApp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ClinicalApp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ClinicalApp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ClinicalApp/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b3d499781502e220e4de05fd388ae3b7",
+    "url": "/ClinicalApp/static/media/slide4.b3d49978.jpg"
+  },
+  {
+    "revision": "c04996a6b1c4ffb63378a4f99cb17959",
+    "url": "/ClinicalApp/static/media/team1.c04996a6.jpg"
+  },
+  {
+    "revision": "5d30275017a20db1503a196537b0274a",
+    "url": "/ClinicalApp/static/media/team2.5d302750.jpg"
+  },
+  {
+    "revision": "0e0ea9e9f4771c3e00fc9ea71bf53740",
+    "url": "/ClinicalApp/static/media/team3.0e0ea9e9.jpg"
+  },
+  {
+    "revision": "8ebe03a0dacafc99c12c04985d691c63",
+    "url": "/ClinicalApp/static/media/team4.8ebe03a0.jpg"
+  }
+]);
